d72907463e3611efa661251311333bd7fc285e9d
Douglas Wille <willedouglas10@gmail.com>
2019-01-16 09:40:36 +0000

    Adjusted url path to https

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = {
-	"api_url": process.env.API_URL || "http://prison-go.herokuapp.com",
+	"api_url": process.env.API_URL || "https://prison-go.herokuapp.com",
 	"request_attempts": process.env.REQUEST_ATTEMPTS || 3
 };
