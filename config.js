206a4aebfe73cc792a0a1886929d7eaeb7b1c201
Douglas Wille <willedouglas10@gmail.com>
2019-04-26 17:58:07 +0000

    changed default url api to production

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-	"api_url": process.env.API_URL || "https://prison-go.herokuapp.com",
+	"api_url": process.env.API_URL || "http://177.2.17.247:3000",
 	"geo_url": process.env.GEO_URL || "https://maps.googleapis.com",
 	"request_attempts": process.env.REQUEST_ATTEMPTS || 3
 };
