66c10b0cbf6e96b619bd0de8260327581f27157e
Douglas Wille <willedouglas10@gmail.com>
2023-06-29 10:07:07 -0300

    chore: updating api url to the new one

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-	"api_url": process.env.API_URL || "http://177.2.17.247:3000",
+	"api_url": process.env.API_URL || "http://45.163.12.94:3000",
 	"geo_url": process.env.GEO_URL || "https://maps.googleapis.com",
 	"request_attempts": process.env.REQUEST_ATTEMPTS || 3
 };
